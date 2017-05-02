ba59185dd160ce333717c6739846e812b4382f14
ashes999 <ashes999@yahoo.com>
2017-05-02 17:57:21 -0400

    First migration (games table)

---
+++ ProtonAnalytics/DatabaseMigrations/001_CreateGameTable.sql
@@ -0,0 +1,7 @@
+﻿create table Games (
+	Id uniqueidentifier primary key not null default newid(),
+	Name nvarchar(255) not null,
+	ApiKey varchar(max) not null,
+	OwnerId nvarchar(128) foreign key references AspNetUsers(Id),
+	CreatedOn datetime not null default getutcdate()	
+)
