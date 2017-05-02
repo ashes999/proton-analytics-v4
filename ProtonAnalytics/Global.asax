afa81352e49b6a82f84448edfcfd46b71a710526
ashes999 <ashes999@yahoo.com>
2017-05-02 17:27:38 -0400

    Brand new ASP.NET MVC4 app

---
+++ ProtonAnalytics/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProtonAnalytics.MvcApplication" Language="C#" %>
---
+++ ProtonAnalytics/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProtonAnalytics.Startup))]
+namespace ProtonAnalytics
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
