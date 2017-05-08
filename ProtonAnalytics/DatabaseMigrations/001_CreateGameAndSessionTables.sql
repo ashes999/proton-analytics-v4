eb63132f073f92fcd2091d5734de59d3b5f3ceab
ashes999 <ashes999@yahoo.com>
2017-05-08 12:40:09 -0400

    Game model: creates API key Empty Game controller SQL: fields mention UTC, limit api key column size

--- ProtonAnalytics/DatabaseMigrations/001_CreateGameAndSessionTables.sql
+++ ProtonAnalytics/DatabaseMigrations/001_CreateGameAndSessionTables.sql
@@ -1,15 +1,15 @@
 ﻿create table Games (
 	Id uniqueidentifier primary key not null default newid(),
 	Name nvarchar(255) not null,
-	ApiKey varchar(max) not null,
+	ApiKey varchar(64) not null, -- 32-bits base-64 encoded
 	OwnerId nvarchar(128) foreign key references AspNetUsers(Id),
-	CreatedOn datetime not null default getutcdate()	
+	CreatedOnUtc datetime not null default getutcdate()	
 )
 
 create table Sessions (
 	PlayerId uniqueidentifier not null,
-	SessionStart datetime not null,
-	SessionEnd datetime, -- null if we don't know, eg. on Flash
+	SessionStartUtc datetime not null,
+	SessionEndUtc datetime, -- null if we don't know, eg. on Flash
 	[Platform] varchar(32) not null,
-	primary key(PlayerId, SessionStart)
+	primary key(PlayerId, SessionStartUtc)
 )
---
+++ ProtonAnalytics/Models/Game.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProtonAnalytics.Models
+{
+    public class Game
+    {
+        private const int ApiSecretSizeInBytes = 32;
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string ApiKey { get; internal set; }
+        public Guid OwnerId { get; internal set; }
+        public DateTime CreatedOnUtc { get; }
+
+        public Game()
+        {
+            this.Id = Guid.NewGuid();
+            this.CreatedOnUtc = DateTime.UtcNow;            
+        }
+
+        public void GenerateApiKey()
+        {
+            if (string.IsNullOrWhiteSpace(this.ApiKey))
+            {
+                // http://stackoverflow.com/questions/14412132/best-approach-for-generating-api-key#18730859
+                var key = new byte[ApiSecretSizeInBytes];
+                using (var generator = System.Security.Cryptography.RandomNumberGenerator.Create())
+                {
+                    generator.GetBytes(key);
+                }
+                this.ApiKey = Convert.ToBase64String(key);
+            }
+        }
+    }
+}
