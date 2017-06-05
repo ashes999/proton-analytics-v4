5442e22a66faacadc2c4e2a543df014ea8e9a82a
ashes999 <ashes999@yahoo.com>
2017-06-05 14:37:42 -0400

    Fix: Guids no longer always start with 80000000

--- Client/source/Guid.hx
+++ Client/source/Guid.hx
@@ -1,4 +1,5 @@
-// Source: https://gist.github.com/ciscoheat/4b1797fa56648adac163f44186f1823a
+// Based on https://gist.github.com/ciscoheat/4b1797fa56648adac163f44186f1823a
+// Any changes include the previous version as commented-out code
 // TODO: move into Noor
 class Guid
 {
@@ -6,8 +7,23 @@ class Guid
     {
 		// Based on https://gist.github.com/LeverOne/1308368
 		var uid = new StringBuf();
-        var a = 8;
-		uid.add(StringTools.hex(Std.int(Date.now().getTime()), 8));
+        
+		var a = 8;
+		
+		// Original code: uid.add(StringTools.hex(Std.int(Date.now().getTime()), 8));
+		// The original algorithm generates GUIDs that always start with 80000000.
+		// Replace the first eight digits with a random hex number. (Max that fits is 2^32.)
+		
+		// Another complication: Neko ints are 31 bits, everything else is 32 bits
+		#if neko
+		var numBits = 31;
+		#else
+		var numBits = 32;
+		#end
+
+		var aNumber = Std.int(Math.pow(2, numBits) * Math.random());
+		uid.add(StringTools.hex(aNumber, 8));
+
 		while((a++) < 36)
         {
 			uid.add(a*51 & 52 != 0
